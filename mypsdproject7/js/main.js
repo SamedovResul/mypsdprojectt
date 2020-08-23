316a8e531db9d0c46577f0d1a14993f1da6e839e
Resul Semedov <resul.semedov.97@mail.ru>
2020-08-23 16:33:28 +0300

    newproject download

---
+++ mypsdproject7/js/main.js
@@ -0,0 +1,11 @@
+var swiper = new Swiper('.swiper-container', {
+    slidesPerView: 'auto',
+    spaceBetween: 40,
+    centeredSlides: true,
+    grabCursor: true,
+    loop: true,
+    pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    },
+});
